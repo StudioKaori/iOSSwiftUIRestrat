71231dd270795a678c4a01ec6bc1cc9cf7005a0e
Studio Kaori <studiokaori.com@gmail.com>
2022-05-22 13:55:04 +0200

    Initial Commit

---
+++ SwiftUIRestart/SwiftUIRestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRestartApp.swift
+//  SwiftUIRestart
+//
+//  Created by Kaori Persson on 2022-05-22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIRestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
