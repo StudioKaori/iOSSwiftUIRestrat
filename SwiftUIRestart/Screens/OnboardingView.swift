b42291705f26b079a99204cafc3203ffd0cf5ad0
Studio Kaori <studiokaori.com@gmail.com>
2022-05-23 06:39:27 +0200

    Add: header in onboarding

--- SwiftUIRestart/Screens/OnboardingView.swift
+++ SwiftUIRestart/Screens/OnboardingView.swift
@@ -13,16 +13,37 @@ struct OnboardingView: View {
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
     
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Onboarding")
-                .font(.largeTitle)
+        ZStack {
+            // fullscreen bg
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
             
-            Button(action: {
-                isOnboardingViewActive = false
-            }) {
-                Text("Start")
-            }
-        } //: VSTACK
+            VStack(spacing: 20) {
+                // MARK: - header
+                Spacer()
+                
+                VStack(spacing: 0) {
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    
+                    // for long text, wrap with 3 """
+                    Text("""
+                    It's not how much we give but
+                    how much love we put into giving.
+                    """)
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 10)
+                } //: header
+                // MARK: - center
+                
+                // MARK: - hooter
+            } //: VSTACK
+        } //: ZSTACK
     }
 }
 
